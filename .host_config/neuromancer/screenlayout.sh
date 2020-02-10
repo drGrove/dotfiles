5ed1cc410c5f02cfe9aa52df9cafa14da89a1d47
Danny Grove <danny@drgrovellc.com>
2020-02-10 14:09:22 -0800

    Add symlink and bring in screen layouts

--- .host_config/neuromancer/screenlayout.sh
+++ .host_config/neuromancer/screenlayout.sh
@@ -1,2 +1,1 @@
-#!/bin/sh
-xrandr --output HDMI-1 --off --output DP-1 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --output HDMI-2 --mode 3840x2160 --pos 0x0 --rotate normal
+screenlayouts/2-screens.sh
