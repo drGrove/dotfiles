f08fa8f17aef006dcedb4f5518445f58a8603a0e
Danny Grove <danny@manifestcyber.com>
2024-05-07 23:44:11 -0700

    Add nvim-tree and lsp suppport

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,60 +11,90 @@ require('opts')      -- Options
 require('keys')      -- Keymaps
 
 -- PLUGINS
--- require('nvim-tree').setup{
---   respect_buf_cwd = true,
---   tab = {
---     sync = {
---       open = true,
---     }
---   }
--- }
--- require'nvim-web-devicons'.setup {
---  -- your personnal icons can go here (to override)
---  -- you can specify color or cterm_color instead of specifying both of them
---  -- DevIcon will be appended to `name`
---  override = {
---   zsh = {
---     icon = "",
---     color = "#428850",
---     cterm_color = "65",
---     name = "Zsh"
---   }
---  };
---  -- globally enable different highlight colors per icon (default to true)
---  -- if set to false all icons will have the default icon's color
---  color_icons = true;
---  -- globally enable default icons (default to false)
---  -- will get overriden by `get_icons` option
---  default = true;
---  -- globally enable "strict" selection of icons - icon will be looked up in
---  -- different tables, first by filename, and if not found by extension; this
---  -- prevents cases when file doesn't have any extension but still gets some icon
---  -- because its name happened to match some extension (default to false)
---  strict = true;
---  -- same as `override` but specifically for overrides by filename
---  -- takes effect when `strict` is true
---  override_by_filename = {
---   [".gitignore"] = {
---     icon = "",
---     color = "#f1502f",
---     name = "Gitignore"
---   }
---  };
---  -- same as `override` but specifically for overrides by extension
---  -- takes effect when `strict` is true
---  override_by_extension = {
---   ["log"] = {
---     icon = "",
---     color = "#81e043",
---     name = "Log"
---   }
---  };
--- }
--- require('lualine').setup {
---   options = {
---     theme = 'tokyonight'
---   }
--- }
--- 
--- require('nvim-autopairs').setup{}
+require('nvim-tree').setup({
+  respect_buf_cwd = true,
+  sort = {
+    sorter = "case_sensitive",
+  },
+  tab = {
+    sync = {
+      open = true,
+    }
+  },
+  renderer = {
+    group_empty = true,
+  },
+  view = {
+    width = 30,
+  },
+})
+
+require('nvim-web-devicons').setup {
+ -- your personnal icons can go here (to override)
+ -- you can specify color or cterm_color instead of specifying both of them
+ -- DevIcon will be appended to `name`
+ override = {
+  zsh = {
+    icon = "",
+    color = "#428850",
+    cterm_color = "65",
+    name = "Zsh"
+  }
+ };
+ -- globally enable different highlight colors per icon (default to true)
+ -- if set to false all icons will have the default icon's color
+ color_icons = true;
+ -- globally enable default icons (default to false)
+ -- will get overriden by `get_icons` option
+ default = true;
+ -- globally enable "strict" selection of icons - icon will be looked up in
+ -- different tables, first by filename, and if not found by extension; this
+ -- prevents cases when file doesn't have any extension but still gets some icon
+ -- because its name happened to match some extension (default to false)
+ strict = true;
+ -- same as `override` but specifically for overrides by filename
+ -- takes effect when `strict` is true
+ override_by_filename = {
+  [".gitignore"] = {
+    icon = "",
+    color = "#f1502f",
+    name = "Gitignore"
+  }
+ };
+ -- same as `override` but specifically for overrides by extension
+ -- takes effect when `strict` is true
+ override_by_extension = {
+  ["log"] = {
+    icon = "",
+    color = "#81e043",
+    name = "Log"
+  }
+ };
+}
+require('lualine').setup {
+  options = {
+    theme = 'tokyonight'
+  }
+}
+
+require('nvim-autopairs').setup{}
+
+
+local lsp_zero = require('lsp-zero')
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+-- to learn how to use mason.nvim
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {},
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  },
+})
--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -1,5 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
@@ -12,9 +12,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-  -- [Editor]
-  "b0o/mapx.nvim",
-  "gpanders/editorconfig.nvim",
   -- [Theme]
   -- the colorscheme should be available when starting Neovim
   {
@@ -23,9 +20,12 @@ local plugins = {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
+      vim.cmd([[colorscheme tokyonight-night]])
     end,
   },
+  -- [Editor]
+  "b0o/mapx.nvim",
+  "gpanders/editorconfig.nvim",
   "mhinz/vim-startify",
   "DanilaMihailov/beacon.nvim",
   {
@@ -54,6 +54,34 @@ local plugins = {
   -- [[ Languages ]]
   "google/vim-jsonnet",
   "jamessan/vim-gnupg",
+  -- Language Support
+  -- Added this plugin.
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {'williamboman/mason.nvim'},           -- Optional
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},         -- Required
+      {'hrsh7th/cmp-nvim-lsp'},     -- Required
+      {'hrsh7th/cmp-buffer'},       -- Optional
+      {'hrsh7th/cmp-path'},         -- Optional
+      {'saadparwaiz1/cmp_luasnip'}, -- Optional
+      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},             -- Required
+      {'rafamadriz/friendly-snippets'}, -- Optional
+    }
+  },
+   -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+  },
 }
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup(plugins)
