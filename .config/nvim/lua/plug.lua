2d2e8aeb133e31b8159c57c11a6fe8870517254e
Danny Grove <danny@dannygrove.com>
2024-05-10 23:30:54 -0700

    Move setup into lazy config function for different plugins

--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -29,16 +29,89 @@ local plugins = {
   "mhinz/vim-startify",
   "DanilaMihailov/beacon.nvim",
   {
+    "kyazdani42/nvim-web-devicons",
+    config = function()
+      require('nvim-web-devicons').setup {
+       -- your personnal icons can go here (to override)
+       -- you can specify color or cterm_color instead of specifying both of them
+       -- DevIcon will be appended to `name`
+       override = {
+        zsh = {
+          icon = "",
+          color = "#428850",
+          cterm_color = "65",
+          name = "Zsh"
+        }
+       };
+       -- globally enable different highlight colors per icon (default to true)
+       -- if set to false all icons will have the default icon's color
+       color_icons = true;
+       -- globally enable default icons (default to false)
+       -- will get overriden by `get_icons` option
+       default = true;
+       -- globally enable "strict" selection of icons - icon will be looked up in
+       -- different tables, first by filename, and if not found by extension; this
+       -- prevents cases when file doesn't have any extension but still gets some icon
+       -- because its name happened to match some extension (default to false)
+       strict = true;
+       -- same as `override` but specifically for overrides by filename
+       -- takes effect when `strict` is true
+       override_by_filename = {
+        [".gitignore"] = {
+          icon = "",
+          color = "#f1502f",
+          name = "Gitignore"
+        }
+       };
+       -- same as `override` but specifically for overrides by extension
+       -- takes effect when `strict` is true
+       override_by_extension = {
+        ["log"] = {
+          icon = "",
+          color = "#81e043",
+          name = "Log"
+        }
+       };
+      }
+    end,
+  },
+  {
     "kyazdani42/nvim-tree.lua",
     dependencies = {
       "kyazdani42/nvim-web-devicons"
-    }
+    },
+    config = function()
+      require('nvim-tree').setup({
+        respect_buf_cwd = true,
+        sort = {
+          sorter = "case_sensitive",
+        },
+        tab = {
+          sync = {
+            open = true,
+          }
+        },
+        renderer = {
+          group_empty = true,
+        },
+        view = {
+          width = 30,
+        },
+      })
+    end,
   },
   {
     "nvim-lualine/lualine.nvim",
     dependencies = {
       "kyazdani42/nvim-web-devicons"
-    }
+    },
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'tokyonight'
+        }
+      }
+    end,
   },
   -- [[ Dev ]]
   {
@@ -49,8 +122,13 @@ local plugins = {
   "Yggdroot/indentLine",                     -- see indentation
   "tpope/vim-fugitive",                      -- git integration
   "junegunn/gv.vim",                         -- commit history
-  "windwp/nvim-autopairs",                   -- auto close brackets, etc.
-  
+  {
+    "windwp/nvim-autopairs",                   -- auto close brackets, etc.
+    config = function()
+      require('nvim-autopairs').setup{}
+    end,
+  },
+
   -- [[ Languages ]]
   "google/vim-jsonnet",
   "jamessan/vim-gnupg",
@@ -76,7 +154,47 @@ local plugins = {
       -- Snippets
       {'L3MON4D3/LuaSnip'},             -- Required
       {'rafamadriz/friendly-snippets'}, -- Optional
-    }
+    },
+    config = function()
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.on_attach(function(client, bufnr)
+        -- see :help lsp-zero-keybindings
+        -- to learn the available actions
+        lsp_zero.default_keymaps({buffer = bufnr})
+      end)
+
+      -- to learn how to use mason.nvim
+      -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
+      require('mason').setup({})
+      require('mason-lspconfig').setup({
+        ensure_installed = {
+          'autotools_ls', -- make
+          'cmake', -- cmake
+          'dockerls', -- docker
+          'gopls', -- go
+          'jedi_language_server', -- python
+          'jqls', -- jq
+          'jsonnet_ls', -- jsonnet
+          'kotlin_language_server', -- kotlin
+          'lua_ls', -- lua
+          'rust_analyzer', -- rust
+          'sqls', -- sql
+          'taplo', -- toml
+          'terraformls', -- terraform
+          'tflint', -- terraform
+          'tsserver', -- typescript
+          'vimls', -- vim
+          'yamlls', -- yaml
+          'zk', -- markdown
+          'zls', -- zig
+        },
+        handlers = {
+          function(server_name)
+            require('lspconfig')[server_name].setup({})
+          end,
+        },
+      })
+    end,
   },
    -- Treesitter
   {
