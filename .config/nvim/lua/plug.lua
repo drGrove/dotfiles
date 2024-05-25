681485e810532b3c8681bc231ca297d95ef06178
Danny Grove <danny@manifestcyber.com>
2024-05-25 13:49:12 -0700

    Add completion mappings

--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -297,6 +297,18 @@ local plugins = {
           end,
         },
       })
+
+      -- Completion
+      local cmp = require('cmp')
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept the currently selectd item.
+        })
+      })
     end,
   },
    -- Treesitter
