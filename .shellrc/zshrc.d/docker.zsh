17177017b0d6c76f34eae96b5ebe3534e4442058
Danny Grove <danny@dannygrove.com>
2025-03-27 14:20:41 -0700

    Disable notary

--- .shellrc/zshrc.d/docker.zsh
+++ .shellrc/zshrc.d/docker.zsh
@@ -1,3 +1,4 @@
 alias fig='docker-compose'
 alias docker-rm-exited="docker rm $(docker ps -a | grep 'Exited' | awk '{ print $1 }')"
-export DOCKER_CONTENT_TRUST=1
+# Disable Notary since we don't use it
+export DOCKER_CONTENT_TRUST=0
